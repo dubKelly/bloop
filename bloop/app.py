ef1080db88a4f5168b63a5ccc8dc4259a5e9b99f
dubKelly <js.neeb1780@gmail.com>
2018-04-03 23:17:21 -0400

    added argparser

--- bloop/app.py
+++ bloop/app.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 
+from argparse import ArgumentParser
 import sys
 
 
-def main():
-  print("running")
+def main(args):
+  parser = ArgumentParser(
+    prog='bloop',
+    description='Auto populate spreadsheet with .txtrpt data'
+  )
+  parser.add_argument(
+    dest='filename',
+    metavar='FILE',
+    nargs=1,
+    help='dump file location'
+  )
+
+  args = parser.parse_args()
+  print(args.filename)
 
 
 def run():
