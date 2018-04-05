a6ecdbdb404c008947bbfde904c296d40d799a0d
dubKelly <js.neeb1780@gmail.com>
2018-04-04 22:13:16 -0400

    printing dump data

--- bloop/app.py
+++ bloop/app.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
+import openpyxl
+import os
 import sys
 
+def scrape(path):
+  for filename in os.listdir(path[0]):
+    location = f'{path[0]}/{filename}'
+    with open(location, 'r') as dump:
+      text = dump.read()
+      print(text)
 
 def main(args):
   parser = ArgumentParser(
@@ -10,15 +18,14 @@ def main(args):
     description='Auto populate spreadsheet with .txtrpt data'
   )
   parser.add_argument(
-    dest='filename',
+    dest='filepath',
     metavar='FILE',
     nargs=1,
     help='dump file location'
   )
 
   args = parser.parse_args()
-  print(args.filename)
-
+  scrape(args.filepath)
 
 def run():
   sys.exit(main(sys.argv[1:]))
